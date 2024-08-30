c47c9250a896374a15bcfc7a4d7341c1e9e65599
AYMAN <126827455+AymanMady@users.noreply.github.com>
2024-08-30 12:00:28 +0000

    first step

---
+++ backend/backend/src/main/java/_4/mr/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package _4.mr.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
